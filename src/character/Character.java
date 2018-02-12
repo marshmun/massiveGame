07f75aa4783f7bf2a570550efef00a5ec027d540
Marshall Munoz <Marshall.Munoz@fbitn.com>
2018-02-12 13:33:15 -0600

    Character class ful-filled

--- src/character/Character.java
+++ src/character/Character.java
@@ -1,4 +1,69 @@
 package character;
 
 public class Character {
-}
+
+    public String name;
+    public String playerClass;
+    public int maxHp;
+    public int maxAtt;
+    public int minAtt;
+    public int level;
+    public int curHp;
+
+    public Character(String name, String playerClass) {
+        this.name = name;
+        this.playerClass = playerClass;
+    }
+
+    public int attack(Character other){
+        int att = (int)(Math.random()*(this.maxAtt - this.minAtt + 1) + this.minAtt);
+        other.curHp -= att;
+        return att;
+    }
+
+    public boolean isDead(){
+        if(this.curHp <= 0){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public int getCurHp(){
+        return curHp;
+    }
+
+    public void setCurHp(int hp){
+        if( hp >= maxHp - curHp){
+            curHp = maxHp;
+        }else{
+            curHp = hp;
+        }
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPlayerClass() {
+        return playerClass;
+    }
+
+    public void setPlayerClass(String playerClass) {
+        this.playerClass = playerClass;
+    }
+
+    public int getMaxHp() {
+        return maxHp;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+   }
--- src/character/Enemy.java
+++ src/character/Enemy.java
@@ -1,4 +1,4 @@
 package character;
 
-public class Enemy {
+public class Enemy extends Character {
 }
