7fd08fa855922cf33fec54df2836bea9552c600e
Marshall Munoz <Marshall.Munoz@fbitn.com>
2018-02-13 08:23:12 -0600

    weapons now are being assinged

--- src/weapon/Staff.java
+++ src/weapon/Staff.java
@@ -1,4 +1,33 @@
 package weapon;
 
-public class Staff {
+public class Staff extends Weapon {
+
+    public static double hp = 2;
+    public static double att = 4;
+    public static String description = "This staffs magic power allows for some defence and even more offence.";
+
+    public Staff() {
+    }
+
+    public static double getHp() {
+        return hp;
+    }
+
+    public static void setHp(double hp) {
+        Staff.hp = hp;
+    }
+
+    public static double getAtt() {
+        return att;
+    }
+
+    public static void setAtt(double att) {
+        Staff.att = att;
+    }
+
+    public static String getDescription() {
+        return description;
+    }
+
+
 }
--- src/weapon/SwordAndShield.java
+++ src/weapon/SwordAndShield.java
@@ -1,4 +1,35 @@
 package weapon;
 
-public class SwordAndShield {
+public class SwordAndShield extends Weapon {
+
+    public static double hp = 3;
+    public static double att= 3;
+    public static String description = "Sword and shield equally balance your defence and attack!";
+
+    public SwordAndShield() {
+    }
+
+    public static double getHp() {
+        return hp;
+    }
+
+    public static void setHp(double hp) {
+        SwordAndShield.hp = hp;
+    }
+
+    public static double getAtt() {
+        return att;
+    }
+
+    public static void setAtt(double att) {
+        SwordAndShield.att = att;
+    }
+
+    public static String getDescription() {
+        return description;
+    }
+
+    public static void setDescription(String description) {
+        SwordAndShield.description = description;
+    }
 }
